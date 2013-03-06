1d96f664f47ea960f378dcc5a05654168199bc90
leikang <leikang@leikang-HP-Compaq-6200-Pro-MT-PC.(none)>
2013-03-06 14:03:43 +0800

    整理代码，删除多余注释，代码等

--- NTC-zCH/src/com/nike/ntc_cn/googleplus/VerticalGooglePlusLayout.java
+++ NTC-zCH/src/com/nike/ntc_cn/googleplus/VerticalGooglePlusLayout.java
@@ -4,23 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.widget.ScrollView;
-
-/*
- * Copyright (C) 2013 Ahmed Nammari
- * 
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ 
 public class VerticalGooglePlusLayout extends ScrollView implements
 		ScrollHandlerInterface {
 
@@ -45,7 +29,7 @@ public class VerticalGooglePlusLayout extends ScrollView implements
 	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
 		super.onScrollChanged(l, t, oldl, oldt);
 
-		final boolean isQuick = (t-oldt) <40;
+		final boolean isQuick = (t-oldt) < 40;
 		
 		if (mScrollerHandler != null) {
 			mScrollerHandler.onScrollChange(isQuick);
