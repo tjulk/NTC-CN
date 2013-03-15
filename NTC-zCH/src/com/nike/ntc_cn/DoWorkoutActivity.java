242e07f470deec376e109366ba053e1665d11ffa
leikang <leikang@baidu.com>
2013-03-15 18:42:04 +0800

    打了一天酱油。。。

--- NTC-zCH/src/com/nike/ntc_cn/DoWorkoutActivity.java
+++ NTC-zCH/src/com/nike/ntc_cn/DoWorkoutActivity.java
@@ -2,16 +2,23 @@ package com.nike.ntc_cn;
 
 import java.util.List;
 
-import com.nike.ntc_cn.db.T_WorkoutExercisesControl;
-import com.nike.ntc_cn.db.T_ExerciseControl.M_Exercises;
-
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.nike.ntc_cn.db.T_ExerciseControl.M_Exercises;
+import com.nike.ntc_cn.db.T_WorkoutExercisesControl;
 
 
 public class DoWorkoutActivity extends BaseActivity{
 	
 	private String workoutName ;
 	private List<M_Exercises> exercises ;
+	
+	private VideoView videoView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,16 @@ public class DoWorkoutActivity extends BaseActivity{
 		
 		workoutName = getIntent().getStringExtra(TutorialListActivity.TAG_WORKOUT_NAME);
 		exercises = T_WorkoutExercisesControl.getInstance(this).getExercisesListByWorkoutname(workoutName);
+		
+		videoView = (VideoView)findViewById(R.id.video_view);
+		Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath()+"/DCIM/Camera/VID_20130315_175030.mp4");  
+		
+		
+		
+		videoView.setMediaController(new MediaController(this));    
+		videoView.setVideoURI(uri);  
+		videoView.start();    
+		videoView.requestFocus();
 		
 		
 	}
